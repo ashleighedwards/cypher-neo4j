27571729a94edde1b1018c7b393ec3630310c192
Ashleigh Edwards <44674311+ashleighedwards@users.noreply.github.com>
2026-05-21 14:00:48 +0000

    adding data using cypher

--- wizardgraph/src/main/java/com/wizardgraph/config/DataLoader.java
+++ wizardgraph/src/main/java/com/wizardgraph/config/DataLoader.java
@@ -20,11 +20,27 @@ public class DataLoader {
                     MERGE (ron:Character {name: 'Ron Weasley', house: 'Gryffindor'})
                     MERGE (draco:Character {name: 'Draco Malfoy', house: 'Slytherin'})
                     MERGE (luna:Character {name: 'Luna Lovegood', house: 'Ravenclaw'})
-                    
+                    MERGE (voldemort:Character {name: 'Lord Voldemort', house: 'Slytherin'})
+                    MERGE (snape:Character {name: 'Severus Snape', house: 'Slytherin'})
+                    MERGE (dumbledore:Character {name: 'Albus Dumbledore', house: 'Gryffindor'})
+
                     MERGE (harry)-[:FRIENDS_WITH]->(hermione)
                     MERGE (harry)-[:FRIENDS_WITH]->(ron)
                     MERGE (hermione)-[:FRIENDS_WITH]->(ron)
                     MERGE (draco)-[:FRIENDS_WITH]->(luna)
+
+                    MERGE (harry)-[:ENEMY_OF]->(draco)
+                    MERGE (harry)-[:ENEMY_OF]->(voldemort)
+                    MERGE (hermione)-[:ENEMY_OF]->(voldemort)
+                    MERGE (ron)-[:ENEMY_OF]->(voldemort)
+                    MERGE (draco)-[:ENEMY_OF]->(harry)
+                    MERGE (luna)-[:ENEMY_OF]->(draco)
+
+                    MERGE (snape)-[:ALLY_OF]->(voldemort)
+                    MERGE (dumbledore)-[:ALLY_OF]->(harry)
+                    MERGE (dumbledore)-[:ALLY_OF]->(hermione)
+                    MERGE (dumbledore)-[:ALLY_OF]->(ron)
+                    
                 """);
 
                 System.out.println("Sample data loaded into Neo4j.");
